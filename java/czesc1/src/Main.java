e940c9de0a2476dca740c131dcbf70e966e34191
pm1975 <pm1975@windowslive.com>
2022-01-11 22:58:56 +0100

    init before learn www,css,js,php

--- java/czesc1/src/Main.java
+++ java/czesc1/src/Main.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;/**
-* Created by Piotr on 2021-03-13
-*/public class Main {
+/**
+ * Created by Piotr on 2021-03-13
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
 }
